9766afd428c2934d4f55df9b150f0128fb1f8165
Khushi sharma <khushisharma1april2005@gmail.com>
2026-01-25 21:01:34 +0530

    JS Day 3 pratice

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //Q1.
 // Reverse the array by using push and pop methods.
-let arr = [1, 2, 3, 4, 5];
-let reversedArr = [];
-while (arr.length > 0) {
-    reversedArr.push(arr.pop());
-}
-console.log(reversedArr);
+// let arr = [1, 2, 3, 4, 5];
+// let reversedArr = [];
+// while (arr.length > 0) {
+//     reversedArr.push(arr.pop());
+// }
+// console.log(reversedArr);
 
 // Q2.
 // Remove all the negative elements from the array
@@ -19,4 +19,24 @@ while(Arr.length>0){
 }
 console.log(posIn);
 
-arr=[1,2,2,1]
+// Q3. check palindrome series
+function palindrome() {
+let arr=[1,2,3,2,1]
+let original=[]
+let reverse=[]
+for (let i=0; i<arr.length; i++) {
+    original.push(arr[i])
+}   
+
+while(arr.length>0){
+    reverse.push(arr.pop())
+}
+for (let i=0; i < original.length; i++){
+    if (original[i] !== reverse[i]) {
+        return "not a palindrome";
+    }
+    return "palindrome";
+}
+console.log(palindrome())
+}
+
